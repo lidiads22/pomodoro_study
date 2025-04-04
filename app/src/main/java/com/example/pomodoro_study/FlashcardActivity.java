7462211f424d3e77ebafbbc09614981bc3b2db07
Lidia <Lidiads22@icloud.com>
2025-04-04 00:43:26 -0500

    Font updates

--- app/src/main/java/com/example/pomodoro_study/FlashcardActivity.java
+++ app/src/main/java/com/example/pomodoro_study/FlashcardActivity.java
@@ -1,24 +1,33 @@
 package com.example.pomodoro_study;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FlashcardActivity extends AppCompatActivity {
 
+    // Textviews from xml
+    private TextView questionTextView;
+    private TextView answerTextView;
+    private CardView flashcardView;
+    private TextView flashcardContent;
+    private ProjectDataBaseHelper dbHelper;
+    private boolean isAnswerShown = false;
+    private ProjectDataBaseHelper.Flashcard currentFlashcard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_flashcard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        questionTextView = findViewById(R.id.questionTextView);
+        answerTextView = findViewById(R.id.answerTextView);
+        flashcardView = findViewById(R.id.flashcardView);
     }
 }
